a10188547c7666ae134984f01b837d5a0c765678
alexdunlop81 <alexdunlop81@yahoo.com>
2014-08-26 20:21:13 +0100

    Geoblock error handling

--- resources/lib/_main_viacom.py
+++ resources/lib/_main_viacom.py
@@ -31,6 +31,7 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 	video_url6 = 'stack://'
 	sbitrate = int(_addoncompat.get_setting('quality'))
 	closedcaption = []
+	exception = False
 	if 'feed' not in video_url:
 		swf_url = _connection.getRedirect(video_url, header = {'Referer' : BASE})
 		params = dict(item.split("=") for item in swf_url.split('?')[1].split("&"))
@@ -38,88 +39,95 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 		config_url = urllib.unquote_plus(params['CONFIG_URL'].replace('Other',DEVICE))
 		config_data = _connection.getURL(config_url, header = {'Referer' : video_url, 'X-Forwarded-For' : '12.13.14.15'})
 		config_tree = BeautifulSoup(config_data, 'html5lib')
-		feed_url = config_tree.feed.string
-		feed_url = feed_url.replace('{uri}', uri).replace('&amp;', '&').replace('{device}', DEVICE).replace('{ref}', 'None').strip()
+		if not config_tree.error:
+			feed_url = config_tree.feed.string
+			feed_url = feed_url.replace('{uri}', uri).replace('&amp;', '&').replace('{device}', DEVICE).replace('{ref}', 'None').strip()
+		else:
+			exception = True
+			error_text = config_tree.error.string.split('/')[-1].split('_') 
+			dialog = xbmcgui.Dialog()
+			dialog.ok("Exception", error_text[1], error_text[2])
 	else:
 		feed_url = video_url
 
-	feed_data = _connection.getURL(feed_url)
-	video_tree = BeautifulSoup(feed_data, 'html.parser', parse_only = SoupStrainer('media:group'))
-	video_segments = video_tree.find_all('media:content')
-	
+	if not exception:
+		feed_data = _connection.getURL(feed_url)
+		video_tree = BeautifulSoup(feed_data, 'html.parser', parse_only = SoupStrainer('media:group'))
+		video_segments = video_tree.find_all('media:content')
+		
 
-	for act, video_segment in enumerate(video_segments):
-		video_url3 = video_segment['url'].replace('{device}', DEVICE)
-		video_data3 = _connection.getURL(video_url3, header = {'X-Forwarded-For' : '12.13.14.15'})
-		video_tree3 = BeautifulSoup(video_data3, 'html5lib')
-		try:
-			duration = video_tree3.find('rendition')['duration']
-			closedcaption.append((video_tree3.find('typographic', format = 'ttml'),duration))
-		except:
-			pass
+		for act, video_segment in enumerate(video_segments):
+			video_url3 = video_segment['url'].replace('{device}', DEVICE)
+			video_data3 = _connection.getURL(video_url3, header = {'X-Forwarded-For' : '12.13.14.15'})
+			video_tree3 = BeautifulSoup(video_data3, 'html5lib')
+			try:
+				duration = video_tree3.find('rendition')['duration']
+				closedcaption.append((video_tree3.find('typographic', format = 'ttml'),duration))
+			except:
+				pass
 
-		video_menu = video_tree3.find('src').string
-		hbitrate = -1
-		lbitrate = -1
+			video_menu = video_tree3.find('src').string
+			hbitrate = -1
+			lbitrate = -1
 
-		m3u_master_data = _connection.getURL(video_menu, savecookie = True)
-		m3u_master = _m3u8.parse(m3u_master_data)
-		hbitrate = -1
-		sbitrate = int(_addoncompat.get_setting('quality')) * 1024
-		for video_index in m3u_master.get('playlists'):
-			bitrate = int(video_index.get('stream_info')['bandwidth'])
-			if qbitrate is None:
-				if bitrate > hbitrate and bitrate <= sbitrate:
-					hbitrate = bitrate
+			m3u_master_data = _connection.getURL(video_menu, savecookie = True)
+			m3u_master = _m3u8.parse(m3u_master_data)
+			hbitrate = -1
+			sbitrate = int(_addoncompat.get_setting('quality')) * 1024
+			for video_index in m3u_master.get('playlists'):
+				bitrate = int(video_index.get('stream_info')['bandwidth'])
+				if qbitrate is None:
+					if bitrate > hbitrate and bitrate <= sbitrate:
+						hbitrate = bitrate
+						m3u8_url =  video_index.get('uri')
+				elif  bitrate == qbitrate:
 					m3u8_url =  video_index.get('uri')
-			elif  bitrate == qbitrate:
-				m3u8_url =  video_index.get('uri')
+				
+			m3u_data = _connection.getURL(m3u8_url, loadcookie = True)
+			key_url = re.compile('URI="(.*?)"').findall(m3u_data)[0]
+			key_data = _connection.getURL(key_url, loadcookie = True)		
+			key_file = open(_common.KEYFILE + str(act), 'wb')
+			key_file.write(key_data)
+			key_file.close()
+			video_url5 = re.compile('(http:.*?)\n').findall(m3u_data)
+			for i, video_item in enumerate(video_url5):
+				newurl = base64.b64encode(video_item)
+				newurl = urllib.quote_plus(newurl)
+				m3u_data = m3u_data.replace(video_item, 'http://127.0.0.1:12345/foxstation/' + newurl)
 			
-		m3u_data = _connection.getURL(m3u8_url, loadcookie = True)
-		key_url = re.compile('URI="(.*?)"').findall(m3u_data)[0]
-		key_data = _connection.getURL(key_url, loadcookie = True)		
-		key_file = open(_common.KEYFILE + str(act), 'wb')
-		key_file.write(key_data)
-		key_file.close()
-		video_url5 = re.compile('(http:.*?)\n').findall(m3u_data)
-		for i, video_item in enumerate(video_url5):
-			newurl = base64.b64encode(video_item)
-			newurl = urllib.quote_plus(newurl)
-			m3u_data = m3u_data.replace(video_item, 'http://127.0.0.1:12345/foxstation/' + newurl)
-		
-		m3u_data = m3u_data.replace(key_url, 'http://127.0.0.1:12345/play.key' + str(act))
+			m3u_data = m3u_data.replace(key_url, 'http://127.0.0.1:12345/play.key' + str(act))
 
 
-		playfile = open(_common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8'), 'w')
-		playfile.write(m3u_data)
-		playfile.close()
-		video_url6 +=  _common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8') + ' , '
-		
-	filestring = 'XBMC.RunScript(' + os.path.join(_common.LIBPATH,'_proxy.py') + ', 12345)'
-	xbmc.executebuiltin(filestring)
-	finalurl = video_url6[:-3]
-	localhttpserver = True
-	time.sleep(20)
+			playfile = open(_common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8'), 'w')
+			playfile.write(m3u_data)
+			playfile.close()
+			video_url6 +=  _common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8') + ' , '
+			
+		filestring = 'XBMC.RunScript(' + os.path.join(_common.LIBPATH,'_proxy.py') + ', 12345)'
+		xbmc.executebuiltin(filestring)
+		finalurl = video_url6[:-3]
+		localhttpserver = True
+		time.sleep(20)
 
-	if (_addoncompat.get_setting('enablesubtitles') == 'true') and (closedcaption is not None):
-		convert_subtitles(closedcaption)
-	item = xbmcgui.ListItem(path = finalurl)
-	if qbitrate is not None:
-		item.setThumbnailImage(_common.args.thumb)
-		item.setInfo('Video', {	'title' : _common.args.name,
-						'season' : _common.args.season_number,
-						'episode' : _common.args.episode_number,
-						'TVShowTitle' : _common.args.show_title})
-	xbmcplugin.setResolvedUrl(pluginHandle, True, item)
-	for count in range(1, act):
-		while not xbmc.Player().isPlaying():
-			xbmc.sleep(200)
-		if (_addoncompat.get_setting('enablesubtitles') == 'true') and (closedcaption is not None) and closedcaption != []:
-			xbmc.Player().setSubtitles(os.path.join(_common.CACHEPATH, 'subtitle-%s.srt' % str(count)))
-			# wait for 20s to load the next segment (but not for the first segment, where this sleep is not needed)
-		while xbmc.Player().isPlaying():
-			xbmc.sleep(10)
-	_connection.getURL('http://localhost:12345/stop', connectiontype = 0)
+		if (_addoncompat.get_setting('enablesubtitles') == 'true') and (closedcaption is not None):
+			convert_subtitles(closedcaption)
+		item = xbmcgui.ListItem(path = finalurl)
+		if qbitrate is not None:
+			item.setThumbnailImage(_common.args.thumb)
+			item.setInfo('Video', {	'title' : _common.args.name,
+							'season' : _common.args.season_number,
+							'episode' : _common.args.episode_number,
+							'TVShowTitle' : _common.args.show_title})
+		xbmcplugin.setResolvedUrl(pluginHandle, True, item)
+		for count in range(1, act):
+			while not xbmc.Player().isPlaying():
+				xbmc.sleep(200)
+			if (_addoncompat.get_setting('enablesubtitles') == 'true') and (closedcaption is not None) and closedcaption != []:
+				xbmc.Player().setSubtitles(os.path.join(_common.CACHEPATH, 'subtitle-%s.srt' % str(count)))
+				# wait for 20s to load the next segment (but not for the first segment, where this sleep is not needed)
+			while xbmc.Player().isPlaying():
+				xbmc.sleep(10)
+		_connection.getURL('http://localhost:12345/stop', connectiontype = 0)
 	
 
 def list_qualities(BASE, video_url = _common.args.url, media_base = VIDEOURL):
