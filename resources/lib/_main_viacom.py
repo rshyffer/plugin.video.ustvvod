04b68a9d69fe85db74b68284ebee98cb324c476d
z-e-r-o <michael.minkwitz@outlook.com>
2014-10-18 18:00:30 +0200

    VH1 fix  +  MTV changes
    All changes are done for both vh1 and mtv (by the way vh1 was not
    working :) )
    + include all definitions to _main_viacom
    + change the request way for videos to the api way
    + include threading of url request - THIS SHOULD IMPROVE SPEED ALOT!
    +

--- resources/lib/_main_viacom.py
+++ resources/lib/_main_viacom.py
@@ -7,8 +7,11 @@ import _m3u8
 import base64
 import datetime
 import os
+import Queue
 import re
+import simplejson
 import sys
+import threading
 import time
 import urllib
 import xbmc
@@ -20,8 +23,92 @@ pluginHandle = int(sys.argv[1])
 player = _common.XBMCPlayer()
 
 VIDEOURL = 'http://media.mtvnservices.com/'
+VIDEOURLAPI = 'http://media-utils.mtvnservices.com/services/MediaGenerator/%s?device=Xbox'
+TYPES = [('fullEpisodes' , 'Full Episodes'), ('bonusClips,afterShowsClips,recapsClips,sneakPeeksClips,dailies,showClips' , 'Extras')]
 DEVICE = 'Xbox'
 BITRATERANGE = 10
+
+def masterlist(SITE, SHOWS):
+	master_db = []
+	master_data = _connection.getURL(SHOWS)
+	master_tree = simplejson.loads(master_data)
+	for master_item in master_tree['promoList']['promos']:
+		master_item = master_item['promo']['associatedContent']['series']
+		master_name = master_item['title']
+		master_id = master_item['seriesId']
+		if master_id is not None:
+			master_db.append((master_name, SITE, 'seasons', master_id))
+		else:
+			pass
+	return master_db
+
+def seasons(SITE, API):
+	season_id = _common.args.url
+	count = 0
+	for type in TYPES:
+		season_url = API + 'series/' + season_id + '/playlists.json?page=0&pageSize=500&type=' + type[0]
+		season_data = _connection.getURL(season_url)
+		try:
+			season_tree = simplejson.loads(season_data)['series']['playlists']
+			for season_item in season_tree:
+				try:
+					if (season_item['playlist']['distributionPolicies'][0]['distributionPolicy']['policyType'] == 'playable'):
+						count = count + 1
+				except:
+					if (season_item['playlist']['distributionPolicies'][0]['policyType'] == 'playable'):
+						count = count + 1
+				else:
+					count = count
+			if count > 0:
+				_common.add_directory(type[1], SITE, 'videos', season_url)
+		except:
+			pass
+	_common.set_view('seasons')
+
+def videos(SITE):
+	episode_url = _common.args.url
+	episode_data = _connection.getURL(episode_url)
+	episode_tree = simplejson.loads(episode_data)
+	for episode_item in episode_tree['series']['playlists']:
+		show_name = episode_tree['series']['title']
+		episode_item = episode_item['playlist']
+		episode_name = episode_item['headline'].split('|')[-1].strip()
+		try:
+			episode_info = re.compile('s([0-9]).e?([0-9]{0,2}).*').findall(episode_item['title'])
+			try:
+				episode_season, episode_number = episode_info[0]
+			except:
+				episode_season = episode_info
+				episode_number = -1
+		except:
+			episode_season = -1
+			episode_number = -1
+		url = episode_item['id']
+		try:
+			episode_plot = episode_item['subhead']
+		except:
+			episode_plot = ''
+		episode_thumb = episode_item['image']
+		episode_duration = _common.format_seconds(episode_item['duration']['timecode'])
+		u = sys.argv[0]
+		u += '?url="' + urllib.quote_plus(url) + '"'
+		u += '&mode="' + SITE + '"'
+		u += '&sitemode="play"'
+		infoLabels = {	'title' : episode_name,
+						'plot' : episode_plot,
+						'durationinseconds' : episode_duration,
+						'tvshowtitle' : show_name,
+						'season' : episode_season,
+						'episode' : episode_number }
+		try:
+			if (episode_item['distributionPolicies'][0]['distributionPolicy']['policyType'] == 'playable'):
+				_common.add_video(u, episode_name, episode_thumb, infoLabels = infoLabels, quality_mode = 'list_qualities')
+		except:
+			if (episode_item['distributionPolicies'][0]['policyType'] == 'playable'):
+				_common.add_video(u, episode_name, episode_thumb, infoLabels = infoLabels, quality_mode = 'list_qualities')
+		else:
+			pass
+	_common.set_view('episodes')
 
 def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 	if media_base not in video_url:
@@ -31,7 +118,6 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 	except:
 		qbitrate = None
 	video_url6 = 'stack://'
-	sbitrate = int(_addoncompat.get_setting('quality'))
 	closedcaption = []
 	exception = False
 	if 'feed' not in video_url:
@@ -55,7 +141,9 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 		feed_url = video_url
 	if not exception:
 		feed_data = _connection.getURL(feed_url)
+		print feed_data
 		video_tree = BeautifulSoup(feed_data, 'html.parser', parse_only = SoupStrainer('media:group'))
+		print video_tree
 		video_segments = video_tree.find_all('media:content')
 		segments = []
 		for act, video_segment in enumerate(video_segments):
@@ -87,9 +175,9 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 							lm3u8_url = video_index.get('uri')
 						if bitrate > hbitrate and bitrate <= sbitrate:
 							hbitrate = bitrate
-							m3u8_url =  video_index.get('uri')
-					elif  (qbitrate  * (100 - BITRATERANGE))/100 <  bitrate  and (qbitrate  * (100 + BITRATERANGE))/100 >  bitrate:
-						m3u8_url =  video_index.get('uri')
+							m3u8_url = video_index.get('uri')
+					elif (qbitrate * (100 - BITRATERANGE))/100 < bitrate and (qbitrate * (100 + BITRATERANGE))/100 > bitrate:
+						m3u8_url = video_index.get('uri')
 				if 	m3u8_url is None and qbitrate is None:
 					m3u8_url = lm3u8_url
 				m3u_data = _connection.getURL(m3u8_url, loadcookie = True)
@@ -104,10 +192,10 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 					newurl = urllib.quote_plus(newurl)
 					m3u_data = m3u_data.replace(video_item, 'http://127.0.0.1:12345/foxstation/' + newurl)
 				m3u_data = m3u_data.replace(key_url, 'http://127.0.0.1:12345/play.key' + str(act))
-				playfile = open(_common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8'), 'w')
+				playfile = open(_common.PLAYFILE.replace('.m3u8', '_' + str(act) + '.m3u8'), 'w')
 				playfile.write(m3u_data)
 				playfile.close()
-				video_url6 +=  _common.PLAYFILE.replace('.m3u8',  '_' + str(act)  + '.m3u8') + ' , '
+				video_url6 += _common.PLAYFILE.replace('.m3u8', '_' + str(act) + '.m3u8') + ' , '
 			except:
 				pass
 		player._segments_array = segments
@@ -129,6 +217,105 @@ def play_video(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 		xbmcplugin.setResolvedUrl(pluginHandle, True, item)
 		while player.is_active:
 			player.sleep(250)
+
+def play_video2(BASE, API, video_url = _common.args.url):
+	try:
+		qbitrate = _common.args.quality
+	except:
+		qbitrate = None
+	video_url2 = 'stack://'
+	threads = []
+	video_queue = Queue.PriorityQueue()
+	closedcaption_queue = Queue.PriorityQueue()
+	segments_queue = Queue.PriorityQueue()
+	video_data = _connection.getURL(API + 'playlists/%s/videos.json' % video_url)
+	video_tree = simplejson.loads(video_data)
+	for i, video_item in enumerate(video_tree['playlist']['videos']):
+		t = threading.Thread(target = get_videos, args = (video_queue, closedcaption_queue, segments_queue, i, video_item, qbitrate))
+		t.setDaemon(True)
+		threads.append(t)
+	[x.start() for x in threads]
+	[x.join() for x in threads]
+	while video_queue.qsize() > 0:
+		video_url2 += video_queue.get() + ' , '
+	player._segments_array = queue_to_list(segments_queue)
+	filestring = 'XBMC.RunScript(' + os.path.join(_common.LIBPATH, '_proxy.py') + ', 12345)'
+	xbmc.executebuiltin(filestring)
+	finalurl = video_url2[:-3]
+	localhttpserver = True
+	time.sleep(20)
+	if (_addoncompat.get_setting('enablesubtitles') == 'true') and (closedcaption_queue.qsize() > 0):
+		convert_subtitles(closedcaption_queue)
+		player._subtitles_Enabled = True
+	item = xbmcgui.ListItem(path = finalurl)
+	if qbitrate is not None:
+		item.setThumbnailImage(_common.args.thumb)
+		item.setInfo('Video', {	'title' : _common.args.name,
+								'season' : _common.args.season_number,
+								'episode' : _common.args.episode_number,
+								'TVShowTitle' : _common.args.show_title })
+	xbmcplugin.setResolvedUrl(pluginHandle, True, item)
+	while player.is_active:
+		player.sleep(250)	
+
+def get_videos(video_queue, closedcaption_queue, segments_queue, i, video_item, qbitrate):
+	lock = threading.Lock()
+	video_mgid = video_item['video']['mgid']
+	video_data = _connection.getURL(VIDEOURLAPI % video_mgid)
+	video_tree = BeautifulSoup(video_data, 'html.parser')
+	try:
+		duration = video_tree.find('rendition')['duration']
+		closedcaption_queue.put((video_tree.find('typographic', format = 'ttml'), duration))
+		segments_queue.put(duration)
+	except:
+		segments_queue.put(0)
+	try:
+		video_menu = video_tree.src.string
+		hbitrate = -1
+		lbitrate = -1
+		m3u8_url = None
+		lock.acquire()
+		m3u8_master_data = _connection.getURL(video_menu, savecookie = True)
+		lock.release()
+		m3u8_master = _m3u8.parse(m3u8_master_data)
+		sbitrate = int(_addoncompat.get_setting('quality')) * 1024
+		for video_index in m3u8_master.get('playlists'):
+			bitrate = int(video_index.get('stream_info')['bandwidth'])
+			if qbitrate is None:
+				if bitrate < lbitrate or lbitrate == -1:
+					lbitrate = bitrate
+					lm3u8_url = video_index.get('uri')
+				if bitrate > hbitrate and bitrate <= sbitrate:
+					hbitrate = bitrate
+					m3u8_url = video_index.get('uri')
+			elif (qbitrate * (100 - BITRATERANGE)) / 100 < bitrate and (qbitrate * (100 + BITRATERANGE)) / 100 > bitrate:
+				m3u8_url = video_index.get('uri')
+		if 	((m3u8_url is None) and (qbitrate is None)):
+			m3u8_url = lm3u8_url
+		m3u8_data = _connection.getURL(m3u8_url, loadcookie = True)
+		key_url = re.compile('URI="(.*?)"').findall(m3u8_data)[0]
+		key_data = _connection.getURL(key_url, loadcookie = True)
+		key_file = open(_common.KEYFILE + str(i), 'wb')
+		key_file.write(key_data)
+		key_file.close()
+		video_url = re.compile('(http:.*?)\n').findall(m3u8_data)
+		for video_item in video_url:
+			newurl = base64.b64encode(video_item)
+			newurl = urllib.quote_plus(newurl)
+			m3u8_data = m3u8_data.replace(video_item, 'http://127.0.0.1:12345/foxstation/' + newurl)
+		m3u8_data = m3u8_data.replace(key_url, 'http://127.0.0.1:12345/play.key' + str(i))
+		playfile = open(_common.PLAYFILE.replace('.m3u8', '_' + str(i) + '.m3u8'), 'w')
+		playfile.write(m3u8_data)
+		playfile.close()
+		video_queue.put(_common.PLAYFILE.replace('.m3u8', '_' + str(i) + '.m3u8'))
+	except:
+		pass
+
+def queue_to_list(q):
+	l = []
+	while q.qsize() > 0:
+		l.append(q.get())
+	return l
 
 def list_qualities(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 	bitrates = []
@@ -169,6 +356,23 @@ def list_qualities(BASE, video_url = _common.args.url, media_base = VIDEOURL):
 			bitrates.append((display, bitrate))
 		return bitrates
 
+def list_qualities2(BASE, API, video_url = _common.args.url):
+	video_bitrates = []
+	video_data = _connection.getURL(API + 'playlists/%s/videos.json' % video_url)
+	video_tree = simplejson.loads(video_data)
+	video_mgid = video_tree['playlist']['videos'][0]['video']['mgid']
+	video_data2 = _connection.getURL(VIDEOURLAPI % video_mgid)
+	video_tree2 = BeautifulSoup(video_data2, 'html.parser')
+	video_menu = video_tree2.find('src').string
+	m3u8_url = None
+	m3u8_master_data = _connection.getURL(video_menu, savecookie = True)
+	m3u8_master = _m3u8.parse(m3u8_master_data)
+	for video_index in m3u8_master.get('playlists'):
+		video_bitrate = int(video_index.get('stream_info')['bandwidth'])
+		bitrate_display = int(video_bitrate) / 1024
+		video_bitrates.append((bitrate_display, video_bitrate))
+	return video_bitrates
+
 def clean_subs(data):
 	br = re.compile(r'<br.*?>')
 	tag = re.compile(r'<.*?>')
@@ -200,8 +404,8 @@ def convert_subtitles(closedcaption):
 				if line is not None:
 					try:
 						sub = clean_subs(_common.smart_utf8(line))
-						start_time = _common.smart_utf8(datetime.datetime.strftime(datetime.datetime.strptime(line['begin'], '%H:%M:%S.%f') -  datetime.timedelta(seconds = int(delay)),'%H:%M:%S,%f'))[:-4]
-						end_time = _common.smart_utf8(datetime.datetime.strftime(datetime.datetime.strptime(line['end'], '%H:%M:%S.%f') -  datetime.timedelta(seconds = int(delay)),'%H:%M:%S,%f'))[:-4]
+						start_time = _common.smart_utf8(datetime.datetime.strftime(datetime.datetime.strptime(line['begin'], '%H:%M:%S.%f') - datetime.timedelta(seconds = int(delay)),'%H:%M:%S,%f'))[:-4]
+						end_time = _common.smart_utf8(datetime.datetime.strftime(datetime.datetime.strptime(line['end'], '%H:%M:%S.%f') - datetime.timedelta(seconds = int(delay)),'%H:%M:%S,%f'))[:-4]
 						str_output += str(j + i + 1) + '\n' + start_time + ' --> ' + end_time + '\n' + sub + '\n\n'
 					except:
 						pass
--- resources/lib/mtv.py
+++ resources/lib/mtv.py
@@ -1,82 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import _common
-import _connection
 import _main_viacom
-import simplejson
-import sys
-import re
-import urllib
-
-pluginHandle = int(sys.argv[1])
 
 SITE = 'mtv'
 NAME = 'MTV'
 DESCRIPTION = "MTV is Music Television. It is the music authority where young adults turn to find out what's happening and what's next in music and popular culture. MTV reaches 412 million households worldwide, and is the #1 Media Brand in the world. Only MTV can offer the consistently fresh, honest, groundbreaking, fun and inclusive youth-oriented programming found nowhere else in the world. MTV is a network that transcends all the clutter, reaching out beyond barriers to everyone who's got eyes, ears and a television set."
 BASE = 'http://www.mtv.com'
-TYPES = [('fullEpisodes' , 'Full Episodes'), ('bonusClips,afterShowsClips,recapsClips,sneakPeeksClips,dailies' , 'Specials')]
-SHOWS = 'http://api.mtv.com/api/vLaNWq0xlbQB/promolist/10393491.json'
+API = 'http://api.mtv.com/api/vLaNWq0xlbQB/'
+SHOWS = API + 'promolist/10393491.json'
 
-def masterlist(master_url = SHOWS):
-	master_db = []
-	master_data = _connection.getURL(SHOWS, header = {'X-Forwarded-For' : '12.13.14.15'})
-	showdata = simplejson.loads(master_data)
-	for data in showdata['promoList']['promos']:
-		data = data['promo']['associatedContent']['series']
-		master_name = data['title']
-		seriesId = data['seriesId']
-		if seriesId is not None:
-			master_db.append((master_name, SITE, 'seasons', seriesId))
-		else: pass
-	return master_db
+def masterlist():
+	return _main_viacom.masterlist(SITE, SHOWS)
 
-def seasons(id = _common.args.url):
-	for type in TYPES:
-		url = 'http://api.mtv.com/api/vLaNWq0xlbQB/series/' + id + '/playlists.json?page=0&pageSize=500&type=' + type[0]
-		data = _connection.getURL(url, header = {'X-Forwarded-For' : '12.13.14.15'})
-		try:
-			count = len(simplejson.loads(data)['series']['playlists'])
-		except:
-			count = 0
-		if count > 0:
-			_common.add_directory(type[1],  SITE, 'videos', url)
-	_common.set_view('seasons')
+def seasons():
+	_main_viacom.seasons(SITE, API)
 
-def videos(episode_url = _common.args.url):
-	episode_data = _connection.getURL(episode_url, header = {'X-Forwarded-For' : '12.13.14.15'})
-	episodes = simplejson.loads(episode_data)
-	for episode in episodes['series']['playlists']:
-		show_name = episodes['series']['title']
-		episode = episode['playlist']
-		episode_name = episode['headline'].split('|')[-1].strip()
-		episode_info = re.compile('s([0-9]).e?([0-9]{0,2}).*').findall(episode['title'])
-		try:
-			episode_season, episode_number = episode_info[0]
-		except:
-			episode_season = episode_info
-			episode_number = -1
-		url = episode['mgid']
-		try:
-			episode_plot = episode['subhead']
-		except:
-			episode_plot = ''
-		episode_thumb = episode['image']
-		episode_duration = _common.format_seconds(episode['duration']['timecode'])
-		u = sys.argv[0]
-		u += '?url="' + urllib.quote_plus(url) + '"'
-		u += '&mode="' + SITE + '"'
-		u += '&sitemode="play"'
-		infoLabels = {	'title' : episode_name,
-						'plot' : episode_plot,
-						'durationinseconds' : episode_duration,
-						'tvshowtitle' : show_name,
-						'season' : episode_season,
-						'episode' : episode_number}
-		_common.add_video(u, episode_name, episode_thumb, infoLabels = infoLabels, quality_mode  = 'list_qualities')
-	_common.set_view('episodes')
+def videos():
+	_main_viacom.videos(SITE)
 
-def play(video_url = _common.args.url):
-	_main_viacom.play_video(BASE, video_url)
+def play():
+	_main_viacom.play_video2(BASE, API, _common.args.url)
 
-def list_qualities(video_url = _common.args.url):
-	return _main_viacom.list_qualities(BASE, video_url)
+def list_qualities():
+	return _main_viacom.list_qualities2(BASE, API, _common.args.url)
