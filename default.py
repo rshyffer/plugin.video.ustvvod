6b00ef9e48dc3e5f7c0630b527fe57b0dc7eda4e
z-e-r-o <michael.minkwitz@outlook.com>
2014-10-23 21:26:35 +0200

    update version string
    @moneymaker365
    Please update also this string for release. Thank you.

--- default.py
+++ default.py
@@ -14,7 +14,7 @@ pluginHandle = int(sys.argv[1])
 __plugin__ = 'USTV VoD'
 __authors__ = 'BlueCop'
 __credits__ = 'moneymaker, slices, zero'
-__version__ = '1.0.9'
+__version__ = '1.1.1'
 
 print '\n\n\n start of USTV VoD plugin'
 
