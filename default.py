ca752aa97e4385e63f326b9b15e04ec0475ce399
moneymaker365 <Tomek@MB14.local>
2014-11-21 14:47:12 +0100

    updated

--- default.py
+++ default.py
@@ -14,7 +14,7 @@ pluginHandle = int(sys.argv[1])
 __plugin__ = 'USTV VoD'
 __authors__ = 'BlueCop'
 __credits__ = 'moneymaker, slices, zero'
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 print '\n\n\n start of USTV VoD plugin'
 
